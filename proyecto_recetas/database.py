dfb739220111efc01aa95c65659715e189723df1
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2026-03-31 23:26:53 -0500

    configurar secretos

--- proyecto_recetas/database.py
+++ proyecto_recetas/database.py
@@ -1,14 +1,19 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-# conexión a MongoDB Atlas
-MONGO_URI = "mongodb+srv://pacoandres03_db_user:admin@blueteam.biz5ysx.mongodb.net/?appName=BlueTeam"
+# cargar variables
+load_dotenv()
+
+# conexión segura
+MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
 
-# base de datos
-db = client["recetafacil"]
+# base de datos (opcional si ya viene en la URI)
+db = client.get_database()
 
 # colección
 users_collection = db["users"]
 
-print("✅ Conectado a MongoDB")
+print("✅ Conectado a MongoDB Atlas")
